ea189fb1629b45b15b35d3df66f062b5b158ae5d
Wang, ChenYu git config --global  git config --global user.email <cwang929@insite.4cd.edu>
2022-10-17 20:23:07 -0700

    7-11

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
 
+
+import matplotlib.pyplot as plt
+import numpy as np
 Math = [100, 90]
 English = [90, 80]
 Physics = [80, 80]
@@ -13,6 +14,17 @@ names = ['Bill', 'Mary']
 # ******************************
 # Make your code
 # ******************************
-
-
+fig,ax = plt.subplots()
+rec1 = ax.bar(x-width-width/2, Math, width, label="Math")
+rec2 = ax.bar(x-width/2, English, width, label="English")
+rec3 = ax.bar(x+width/2, Computer, width, label="Computer")
+rec4 = ax.bar(x+width+width/2, Physics, width, label="Physics")
+ax.set_title("Grouped graph for scores")
+ax.legend()
+ax.set_xticks(x, names)
+ax.bar_label(rec1, padding=3)
+ax.bar_label(rec2, padding=3)
+ax.bar_label(rec3, padding=3)
+ax.bar_label(rec4, padding=3)
+plt.show()
 fig.savefig('A11.png')
